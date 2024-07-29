c5fd1295f164b69daebd66264bfcfb5227f143c9
Diogo José Freitas de Oliveira <djfreitasoliveira@gmail.com>
2024-07-28 22:25:01 -0400

    Funcoes básicas e primeira interface.

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,29 @@
 class Calculadora():
-	def __init__():
+	def __init__(self):
+		print('-='*30)
+		print('Inicializando a calculadora...')
+		print('-='*30)
 		pass
-	def exec():
+	def exec(self, calculo:str):
 		# Este código deve apenas receber os números e operadores e executar as funćoes atribuidas a cada conta
-		pass
+		n1, op, n2 = calculo.split(' ')
+		if op == "+":
+			return n1+n2
+		elif op == "-":
+			return n1-n2
+		elif op == "*":
+			return n1*n2
+		elif op == "/":
+			return n1/n2
+		else:
+			print('Operador ou formato da conta incoerente.')
+		return 0
+	
+if __name__ == '__main__':
 
+	c = Calculadora()
+
+	r = 0
+	while r!=-1:
+		r = str(input('Insira a operação desejada, seguindo o modelo (<número> <operador> <numero>): '))
+		print(c.exec(r))
