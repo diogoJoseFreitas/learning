202c92ec8f56f2a6909a46dd302c9f13c6185337
Diogo José Freitas de Oliveira <djfreitasoliveira@gmail.com>
2024-07-29 21:20:14 -0400

    Sugestão de Criação

---
+++ python/interfaces/tkcalculadora.py
@@ -0,0 +1,5 @@
+from python.models.calculadora import Calculadora
+import tkinter as tk
+
+# Para futuramente criar uma interface usando Tkinter.
+# Visando demonstrar a versatilidade de separar os modelos das interfaces.
